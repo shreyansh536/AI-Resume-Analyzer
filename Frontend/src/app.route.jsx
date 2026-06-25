73d5d8138329e9e0c047a9131b8c460be66b2eac
Shrreyanshsrivastava <srivastavashreyansh12345@gmail.com>
2026-06-25 21:59:03 +0530

    Fix case sensitive imports and home file

--- Frontend/src/app.route.jsx
+++ Frontend/src/app.route.jsx
@@ -5,7 +5,7 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 
 import Login from "./features/auth/pages/login.jsx";
 import Register from "./features/auth/pages/register.jsx";
-import Home from "./features/interview/pages/Home.jsx";
+import Home from "./features/interview/pages/home.jsx";
 import Interview from "./features/interview/pages/interview.jsx";
 import ProtectedRoute from "./features/auth/components/protected.jsx";
 
