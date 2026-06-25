cca8597b4d6a8bba7eae21058e851d68747d963b
Shrreyanshsrivastava <srivastavashreyansh12345@gmail.com>
2026-06-25 20:17:29 +0530

    Fix login import case mismatch

--- Frontend/src/app.route.jsx
+++ Frontend/src/app.route.jsx
@@ -1,44 +1,9 @@
  
-// import { createBrowserRouter } from "react-router-dom";
-
-// import Login from "./features/auth/pages/Login.jsx";
-// import Register from "./features/auth/pages/register.jsx";
-// import Home from "./features/interview/pages/Home.jsx";
-// import Interview from "./features/interview/pages/interview.jsx";
-// import ProtectedRoute from "./features/auth/components/protected.jsx";
-
-// export const router = createBrowserRouter([
-//   {
-//     path: "/login",
-//     element: <Login />,
-//   },
-//   {
-//     path: "/register",
-//     element: <Register />,
-//   },
-//   {
-//     path: "/home",
-//     element: (
-//       <ProtectedRoute>
-//         <Home />
-//       </ProtectedRoute>
-//     ),
-//   },
-//   {
-//     path: "/interview/:interviewId",
-//     element: (
-//       <ProtectedRoute>
-//         <Interview />
-//       </ProtectedRoute>
-//     ),
-//   },
-// ]);
-
 
 
 import { createBrowserRouter, Navigate } from "react-router-dom";
 
-import Login from "./features/auth/pages/Login.jsx";
+import Login from "./features/auth/pages/login.jsx";
 import Register from "./features/auth/pages/register.jsx";
 import Home from "./features/interview/pages/Home.jsx";
 import Interview from "./features/interview/pages/interview.jsx";
